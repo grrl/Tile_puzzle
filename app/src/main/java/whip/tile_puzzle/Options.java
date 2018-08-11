dacce475346e00cf74eaa6adec0d57acc906c432
Jonesジョーンズ <jooj@uef.fi>
2018-08-11 18:57:36 +0300

    upd

--- app/src/main/java/whip/tile_puzzle/Options.java
+++ app/src/main/java/whip/tile_puzzle/Options.java
@@ -2,6 +2,8 @@ package whip.tile_puzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Options extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Options extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Options.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.images));
+        //adapter dropdown
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
