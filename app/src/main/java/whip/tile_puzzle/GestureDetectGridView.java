e2acdd602132c265c5a4747cd8e8359798bffa4b
Jonesジョーンズ <jooj@uef.fi>
2018-08-12 22:23:39 +0300

    upd

--- app/src/main/java/whip/tile_puzzle/GestureDetectGridView.java
+++ app/src/main/java/whip/tile_puzzle/GestureDetectGridView.java
@@ -79,48 +79,7 @@ public class GestureDetectGridView extends GridView {
                 //return super.onSingleTapUp(e1);
                 return true;
             }
-
-            /*
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e1) {
-                final int position = GestureDetectGridView.this.pointToPosition(Math.round(e1.getX()), Math.round(e1.getY()));
-
-                MainActivity.moveTiles2(context, position); //tähän jotain
-                return super.onSingleTapConfirmed(e1);
-            }
-            */
-
-            /*
-            @Override //onFling
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                                   float velocityY) {
-                final int position = GestureDetectGridView.this.pointToPosition
-                        (Math.round(e1.getX()), Math.round(e1.getY()));
-
-                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
-                    if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH
-                            || Math.abs(velocityY) < SWIPE_THRESHOLD_VELOCITY) {
-                        return false;
-                    }
-                    if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE) {
-                        MainActivity.moveTiles(context, MainActivity.up, position);
-                    } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE) {
-                        MainActivity.moveTiles(context, MainActivity.down, position);
-                    }
-                } else {
-                    if (Math.abs(velocityX) < SWIPE_THRESHOLD_VELOCITY) {
-                        return false;
-                    }
-                    if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE) {
-                        MainActivity.moveTiles(context, MainActivity.left, position);
-                    } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE) {
-                        MainActivity.moveTiles(context, MainActivity.right, position);
-                    }
-                }
-
-                return super.onFling(e1, e2, velocityX, velocityY);
-            }
-            */
+            
         });
     }
 
