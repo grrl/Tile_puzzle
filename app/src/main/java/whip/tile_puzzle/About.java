dfeab6fbf0eed4a2f48fc7eab47fd10af6edcaa8
Jonesジョーンズ <jooj@uef.fi>
2018-08-13 07:28:29 +0300

    upd

--- app/src/main/java/whip/tile_puzzle/About.java
+++ app/src/main/java/whip/tile_puzzle/About.java
@@ -2,11 +2,13 @@ package whip.tile_puzzle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Locale;
 
@@ -15,6 +17,7 @@ public class About extends AppCompatActivity {
     private Spinner spinner;
     private String selecteditem;
     private ImageView imageView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,12 @@ public class About extends AppCompatActivity {
         Spinner spinner = (Spinner) findViewById(R.id.spinnerAbout);
 
         imageView = (ImageView) findViewById(R.id.imageView12);
+
+        textView = (TextView) findViewById(R.id.textViewAbout);
+
+        textView.setClickable(true);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
 
         final ArrayAdapter<String> adapter;
 
@@ -66,30 +75,39 @@ public class About extends AppCompatActivity {
                     switch (selecteditem){
                         case "Auringonlasku":
                             imageView.setImageResource(R.drawable.p1);
+                            textView.setText(R.string.p1s);
                             break;
                         case "Kukat":
                             imageView.setImageResource(R.drawable.p5);
+                            textView.setText(R.string.p5s);
                             break;
                         case "Kitaristi":
                             imageView.setImageResource(R.drawable.p6);
+                            textView.setText(R.string.p6s);
                             break;
                         case "Pyykit":
                             imageView.setImageResource(R.drawable.p9);
+                            textView.setText(R.string.p9s);
                             break;
                         case "Fuji vuori":
                             imageView.setImageResource(R.drawable.p3);
+                            textView.setText(R.string.p3s);
                             break;
                         case "Temppeli":
                             imageView.setImageResource(R.drawable.p4);
+                            textView.setText(R.string.p4s);
                             break;
                         case "Väsynyt palkansaaja":
                             imageView.setImageResource(R.drawable.p8);
+                            textView.setText(R.string.p8s);
                             break;
                         case "Rautatieasema":
                             imageView.setImageResource(R.drawable.p2);
+                            textView.setText(R.string.p2s);
                             break;
                         case "Myyntiautomaatti":
                             imageView.setImageResource(R.drawable.p7);
+                            textView.setText(R.string.p7s);
                             break;
                         default:
                             break;
@@ -113,31 +131,39 @@ public class About extends AppCompatActivity {
 
                         case "日没":
                             imageView.setImageResource(R.drawable.p1);
+                            textView.setText(R.string.p1s);
                             break;
                         case "花":
                             imageView.setImageResource(R.drawable.p6);
+                            textView.setText(R.string.p6s);
                             break;
                         case "ギタリスト":
                             imageView.setImageResource(R.drawable.p5);
+                            textView.setText(R.string.p5s);
                             break;
                         case "ランドリー":
                             imageView.setImageResource(R.drawable.p9);
+                            textView.setText(R.string.p9s);
                             break;
                         case "フジ":
                             imageView.setImageResource(R.drawable.p3);
+                            textView.setText(R.string.p3s);
                             break;
                         case "寺":
                             imageView.setImageResource(R.drawable.p4);
+                            textView.setText(R.string.p4s);
                             break;
                         case "サラリーマン":
                             imageView.setImageResource(R.drawable.p8);
+                            textView.setText(R.string.p8s);
                             break;
                         case "鉄道の駅":
                             imageView.setImageResource(R.drawable.p2);
+                            textView.setText(R.string.p2s);
                             break;
                         case "自動販売機":
                             imageView.setImageResource(R.drawable.p7);
-
+                            textView.setText(R.string.p7s);
                             break;
                         default:
                             break;
@@ -160,30 +186,39 @@ public class About extends AppCompatActivity {
 
                         case "Flowers":
                             imageView.setImageResource(R.drawable.p5);
+                            textView.setText(R.string.p5s);
                             break;
                         case "Guitarist":
                             imageView.setImageResource(R.drawable.p6);
+                            textView.setText(R.string.p6s);
                             break;
                         case "Laundry":
                             imageView.setImageResource(R.drawable.p9);
+                            textView.setText(R.string.p9s);
                             break;
                         case "Mount Fuji":
                             imageView.setImageResource(R.drawable.p3);
+                            textView.setText(R.string.p3s);
                             break;
                         case "Sunset":
                             imageView.setImageResource(R.drawable.p1);
+                            textView.setText(R.string.p1s);
                             break;
                         case "Temple":
                             imageView.setImageResource(R.drawable.p4);
+                            textView.setText(R.string.p4s);
                             break;
                         case "Tired salaryman":
                             imageView.setImageResource(R.drawable.p8);
+                            textView.setText(R.string.p8s);
                             break;
                         case "Railway station":
                             imageView.setImageResource(R.drawable.p2);
+                            textView.setText(R.string.p2s);
                             break;
                         case "Vending Machine":
                             imageView.setImageResource(R.drawable.p7);
+                            textView.setText(R.string.p7s);
                             break;
                         default:
                             break;
