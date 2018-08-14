ce74e3e3cedbc08a0599b152cc3f8d636d8ecb06
Jonesジョーンズ <jooj@uef.fi>
2018-08-14 20:12:54 +0300

    upd

---
+++ app/src/androidTest/java/whip/tile_puzzle/MainActivityTest.java
@@ -0,0 +1,30 @@
+package whip.tile_puzzle;
+
+import android.view.View;
+import android.widget.GridView;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    private MainActivity m_Activity = null;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testLaunch(){
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        m_Activity = null;
+    }
+}
